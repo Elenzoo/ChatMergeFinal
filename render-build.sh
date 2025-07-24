842ff45cc559986b370b155d678da1360a17787d
Elenzoo <sergiuszzyromski@gmail.com>
2025-07-24 17:23:22 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-echo "📦 Instalacja zależności..."
-npm install
 
-echo "📂 Tworzę cache Puppeteera..."
-export PUPPETEER_CACHE_DIR=".cache/puppeteer"
-
-echo "📥 Pobieram Chromium..."
-npx puppeteer install chromium
-
-echo "✅ Puppeteer Core + Chromium gotowe!"
